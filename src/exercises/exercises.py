f4d25bb8cd8202a6dfd562d7ab1a31ed1124c677
Larisa Agishtein <lagishtein@touro.edu>
2026-04-16 12:21:20 -0400

    Correct function definition for assignments_without_grades
    Fix function definition formatting for assignments_without_grades.

--- src/exercises/exercises.py
+++ src/exercises/exercises.py
@@ -243,7 +243,7 @@ def students_with_average_above(threshold: float) -> list[Student]:
            .all())
 
 
-def assignments_without_grades() -> list[Assignment]:     
+def assignments_without_grades() -> list[Assignment]:
     """TODO: Return assignments that have no grades yet, ordered by title."""     
     return(         
            db.session.query(Assignment)         
